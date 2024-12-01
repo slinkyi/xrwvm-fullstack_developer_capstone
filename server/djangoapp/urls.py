9bf115204590ed62a1c5c0515378afda3708c718
slinkyi <https.www.kizi.com@gmail.com>
2024-11-30 21:27:28 -0500

    module 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,8 +6,9 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # # path for registration
     path(route='register', view=views.registration, name='register'),
@@ -15,7 +16,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     # path for dealer reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
