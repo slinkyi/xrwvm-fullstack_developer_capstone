64570d0f8f860d1f753f681e623b2befef523b58
slinkyi <https.www.kizi.com@gmail.com>
2024-12-01 18:58:59 -0600

    update models.py
    5.1

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
+# from django.utils.timezone import now
+# from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,9 +12,8 @@ class CarMake(models.Model):
     description = models.TextField(blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     established_year = models.IntegerField(blank=True, null=True)
-   
-    def __str__(self):
-        return self.name
+def __str__(self):
+    return self.name
 
 
 class CarModel(models.Model):
@@ -25,7 +24,6 @@ class CarModel(models.Model):
     CONVERTIBLE = 'Convertible'
     COUPE = 'Coupe'
     TRUCK = 'Truck'
-    
     CAR_TYPE_CHOICES = [
         (SEDAN, 'Sedan'),
         (SUV, 'SUV'),
@@ -37,7 +35,14 @@ class CarModel(models.Model):
     ]
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models') # Many-to-one relationship
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default='Sedan')
+    type = models.CharField(max_length=20, 
+                            choices=CAR_TYPE_CHOICES, 
+                            default='Sedan')
     year = models.IntegerField(default=2024)
-    color = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    color = models.CharField(max_length=50, 
+                             blank=True, 
+                             null=True)
+    price = models.DecimalField(max_digits=10, 
+                                decimal_places=2, 
+                                blank=True, 
+                                null=True)
